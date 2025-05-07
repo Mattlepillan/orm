0ff7b605c7b5e705f69d7288b2beaea3fa7cf848
Matt <ma.lepillan@duocuc.cl>
2025-05-07 13:44:42 -0400

    nuevo commit

---
+++ src/main/java/com/seccion7/orm/controller/PacienteController.java
@@ -0,0 +1,28 @@
+package com.seccion7.orm.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.seccion7.orm.model.Paciente;
+import com.seccion7.orm.service.PacienteService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/v1/pacientes")
+public class PacienteController {
+    @Autowired
+    private PacienteService pacienteService;
+
+    @GetMapping("path")
+    public ResponseEntity<List<Paciente>> getAll() {
+        return ResponseEntity<>(pacienteService);
+    }
+    
+    
+}
---
+++ src/main/java/com/seccion7/orm/service/PacienteService.java
@@ -0,0 +1,19 @@
+package com.seccion7.orm.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.seccion7.orm.model.Paciente;
+import com.seccion7.orm.repository.PacienteRepository;
+
+public class PacienteService  {
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public List<Paciente> readAll(){
+        return pacienteRepository.findAll();
+
+    }
+    
+}
